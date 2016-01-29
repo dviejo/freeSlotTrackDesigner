0a378cdf4ccba8ad6f7e8bfa8f42c33ed0e99296
dviejo <dviejo.dccia@gmail.com>
2016-01-29 17:25:24 +0100

    first tracker version. Bugs detected!

--- freeSlotTrackDesigner/src/freeSlotTrackDesigner/Tracker2000Parser.java
+++ freeSlotTrackDesigner/src/freeSlotTrackDesigner/Tracker2000Parser.java
@@ -6,7 +6,12 @@ package freeSlotTrackDesigner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * @author dviejo
@@ -15,11 +20,32 @@ import java.util.Scanner;
 public class Tracker2000Parser {
 
 	private String fileNameInput, fileNameOutput; 
+	private ArrayList<TramoPista> listaTramos;
+	Map<String, Map<Integer, TramoPista>> fabricantes;
 	
 	public Tracker2000Parser(String fileName)
 	{
 		Scanner myScanner;
 		int posExtension;
+		listaTramos = new ArrayList<TramoPista>();
+		
+		//creamos el conjunto de fabricantes
+		fabricantes = new TreeMap<String, Map<Integer,TramoPista>>();
+		
+		//NINCO
+		Map<Integer, TramoPista> ninco = new TreeMap<Integer,TramoPista>();
+		ninco.put(10102, TramoPista.nincoRectaStd);
+		ninco.put(10103, TramoPista.nincoRectaMedia);
+		ninco.put(10104, TramoPista.nincoRectaCuarto);
+		ninco.put(10105, TramoPista.nincoCurvaEstandar);
+		ninco.put(10112, TramoPista.nincoCurvaEstandarMedia);
+		ninco.put(10106, TramoPista.nincoCurvaInterior);
+		ninco.put(10113, TramoPista.nincoCurvaInteriorMedia);
+		ninco.put(10107, TramoPista.nincoCurvaExterior);
+		ninco.put(10108, TramoPista.nincoCurvaSuperExterior);
+		
+		fabricantes.put("NINCO", ninco);
+		
 		
 		if(fileName!=null)
 			fileNameInput = fileName;
@@ -40,16 +66,147 @@ public class Tracker2000Parser {
 	{
 		File outputFile;
 		FileReader inputFile;
+		StreamTokenizer st;
+		int token;
+		int cont, numTramos;
+		int codigoTramo = -1;
+		TramoPista tramoActual;
+		int status;
+		int numTRACKS = -1; //valor 'number of TRACKS' del formato tr3
+		String fabricante;
+		Map<Integer, TramoPista> pistas = new TreeMap<Integer,TramoPista>();
+		int errorCode = 0;
+		
+		float longIzdo, longDcho;
+		longIzdo = longDcho = 0f;
 		
 		try {
 			inputFile = new FileReader(fileNameInput);
+			st = new StreamTokenizer(inputFile);
+			st.commentChar('#');
+			//leemos la cabecera
+			fabricante = leerCadena(st);
+			if(fabricante!=null)
+			{
+				pistas = fabricantes.get(fabricante);
+				if(pistas!=null)
+				{
+					//leemos y desechamos los offsets
+					if(leerFloat(st)!=-1&&leerFloat(st)!=-1)
+					{
+						//leemos el numero de pistas
+						//TODO soportar un numero mayor a 1
+						numTRACKS = leerEntero(st);
+						if(numTRACKS==1)
+						{
+							//leemos y desechamos el angulo y el grupo
+							leerEntero(st);
+							leerFloat(st);
+							leerFloat(st);
+							leerEntero(st);
+							//leemos el numero de tramos
+							numTramos = leerEntero(st);
+							if(numTramos>0)
+							{
+								for(cont=0;cont<numTramos&&errorCode==0;cont++)
+								{
+									codigoTramo = leerEntero(st);
+									if(codigoTramo!=-1)
+									{
+										tramoActual = pistas.get(codigoTramo);
+										if(tramoActual != null)
+										{
+											System.out.print(tramoActual+ " ");
+											//leemos la altura y la desechamos
+											leerEntero(st);
+											//leemos el estado
+											status = leerEntero(st);
+											if(tramoActual.isCurva())
+											{
+												if(status==2) tramoActual.toggleOrientation();
+												if(tramoActual.getCambioOrientacion())
+													System.out.print("Curva Izquierda");
+												else
+													System.out.print("Curva Derecha");
+											}
+											System.out.println();
+										} else errorCode = -7;
+									} else errorCode = -6;
+								} //end for
+							} else errorCode = -5;
+						} else errorCode = -4;
+					} else errorCode = -3;
+				} else errorCode = -2;
+			} else errorCode = -1;
+			
+			switch(errorCode)
+			{
+			case 0:
+				System.out.println("Parser de fichero realizado correctamente");
+				break;
+			case -1:
+				System.out.println("Error en la estructura del fichero (Fabricante)");
+				break;
+			case -2:
+				System.out.println("Fabricante no soportado ("+fabricante+")");
+				break;
+			case -3:
+				System.out.println("Error en la estructura del fichero (Offset)");
+				break;
+			case -4:
+				System.out.println("Error: numTRACKS no soportado. Debe ser 1 y es "+numTRACKS);
+				break;
+			case -5:
+				System.out.println("Error en la estructura del fichero (numTramos)");
+				break;
+			case -6:
+				System.out.println("Error en la estructura del fichero (codigo Tramo: "+codigoTramo+")");
+				break;
+			case -7:
+				System.out.println("No se encuentra el tramo (codigo Tramo: "+codigoTramo+")");
+				break;
+			}
+			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("No se encuentra el fichero: "+fileNameInput);
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return 0;
 	}
 	
+	private int leerEntero(StreamTokenizer t) throws IOException
+	{
+		int ret = -1;
+		int token = t.nextToken();
+		if(token!=StreamTokenizer.TT_NUMBER) t.pushBack();
+		else ret = (int)t.nval;
+		
+		return ret;
+	}
+	
+	private float leerFloat(StreamTokenizer t) throws IOException
+	{
+		float ret = -1;
+		int token = t.nextToken();
+		if(token!=StreamTokenizer.TT_NUMBER) t.pushBack();
+		else ret = (float)t.nval;
+		
+		return ret;
+	}
+
+	private String leerCadena(StreamTokenizer t) throws IOException
+	{
+		String ret = null;
+		int token = t.nextToken();
+		if(token!=StreamTokenizer.TT_WORD) t.pushBack();
+		else ret = t.sval;
+		
+		return ret;
+	}
+
 	/**
 	 * @param args
 	 */
--- freeSlotTrackDesigner/src/freeSlotTrackDesigner/TramoPista.java
+++ freeSlotTrackDesigner/src/freeSlotTrackDesigner/TramoPista.java
@@ -12,17 +12,17 @@ package freeSlotTrackDesigner;
  */
 
 public enum TramoPista {
-	nincoRectaStd(0f, 400.0f, 0f, 400.0f, 400.0f, false),
-	nincoRectaMedia(0f, 200.0f, 0f, 200.0f, 200.0f, false),
-	nincoRectaCuarto(0f, 100.0f, 0f, 100.0f, 100.0f, false),
-	nincoRectaOctavo(0f, 50.0f, 0f, 50.0f, 50.0f, false),
+	nincoRectaStd(0f, 400.0f, 0f, 400.0f, 400.0f, false, false),
+	nincoRectaMedia(0f, 200.0f, 0f, 200.0f, 200.0f, false, false),
+	nincoRectaCuarto(0f, 100.0f, 0f, 100.0f, 100.0f, false, false),
+	nincoRectaOctavo(0f, 50.0f, 0f, 50.0f, 50.0f, false, false),
 	//nincoCambioPista
-	nincoCurvaInterior(44.41f, 107.215f, -45f, 154.429f, 83.743f, false),
-	nincoCurvaInteriorMedia(11.542f, 58.024f, -22.5f, 77.2145f, 41.872f, false),
-	nincoCurvaEstandar(70.77f, 170.855f, -45f, 295.8f, 225.115f, false),
-	nincoCurvaEstandarMedia(18.393f, 92.466f, -22.5f, 147.9f, 112.557f, false),
-	nincoCurvaExterior(38.945f, 195.790f, -22.5f, 218.586f, 183.243f, false),
-	nincoCurvaSuperExterior(52.647f, 264.673f, -22.5f, 289.272f, 253.929f, false);
+	nincoCurvaInterior(44.41f, 107.215f, -45f, 154.429f, 83.743f, false, true),
+	nincoCurvaInteriorMedia(11.542f, 58.024f, -22.5f, 77.2145f, 41.872f, false, true),
+	nincoCurvaEstandar(70.77f, 170.855f, -45f, 295.8f, 225.115f, false, true),
+	nincoCurvaEstandarMedia(18.393f, 92.466f, -22.5f, 147.9f, 112.557f, false, true),
+	nincoCurvaExterior(38.945f, 195.790f, -22.5f, 218.586f, 183.243f, false, true),
+	nincoCurvaSuperExterior(52.647f, 264.673f, -22.5f, 289.272f, 253.929f, false, true);
 	
 	public boolean getCambioOrientacion() {
 		return cambioOrientacion;
@@ -30,6 +30,11 @@ public enum TramoPista {
 
 	public void setCambioOrientacion(boolean cambioOrientacion) {
 		this.cambioOrientacion = cambioOrientacion;
+	}
+	
+	public void toggleOrientation()
+	{
+		cambioOrientacion = !cambioOrientacion;
 	}
 
 	public float getPosX() {
@@ -41,18 +46,28 @@ public enum TramoPista {
 	}
 
 	public float getLongIzquierdo() {
-		return longIzquierdo;
+		return cambioOrientacion?longDerecho:longIzquierdo;
 	}
 
 	public float getLongDerecho() {
-		return longDerecho;
+		return cambioOrientacion?longIzquierdo:longDerecho;
 	}
 
 	private float posX, posY, angulo;
 	private float longIzquierdo, longDerecho;
 	boolean cambioOrientacion;
+	boolean curva;
 	
-	TramoPista(float x, float y, float a, float li, float ld, boolean cambio)
+	/**
+	 * 
+	 * @param x Posición X de la siguiente pista tras insertar este tramo. Expresado en mm.
+	 * @param y Posición Y de la siguiente pista tras insertar este tramo. Expresado en mm.
+	 * @param a Angulo de la pista tras insertar este tramo. Expresado en grados.
+	 * @param li Longitud recorrida por el carril izquierdo. Expresado en mm.
+	 * @param ld Longitud recorrida por el carril derecho. Expresado en mm.
+	 * @param cambio Indica si se produce un cambio en el giro de la recta. False: giro a la derecha, True: giro a la izquierda.
+	 */
+	TramoPista(float x, float y, float a, float li, float ld, boolean cambio, boolean c)
 	{
 		posX = x;
 		posY = y;
@@ -60,6 +75,11 @@ public enum TramoPista {
 		longIzquierdo = li;
 		longDerecho = ld;
 		cambioOrientacion = cambio;
+		curva = c;
+	}
+
+	public boolean isCurva() {
+		return curva;
 	}
 
 	public float getAngulo() {
